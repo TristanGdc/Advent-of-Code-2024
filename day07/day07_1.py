4dc824b7a2b0afbb9955229da797e4e191e197d7
Tristan Godec <tristan.godec@gmail.com>
2025-03-03 18:40:56 +0100

    improve day 7

--- day07/day07_1.py
+++ day07/day07_1.py
@@ -1,9 +1,3 @@
-def generate_binaries(n: int):
-    if n>1:
-        return [bin+[0] for bin in generate_binaries(n-1)] + [bin+[1] for bin in generate_binaries(n-1)]
-    else:
-        return [[0], [1]]
-
 with open("Advent-of-Code-2024/inputs/input07") as f:
     equations = [[int(eq[0]), [int(i) for i in eq[1].strip().split(' ')]] for eq in [l.strip().split(':') for l in f.readlines()]]
 
@@ -11,21 +5,13 @@ with open("Advent-of-Code-2024/inputs/input07") as f:
 
     for eq in equations:
 
-        for operations in generate_binaries(len(eq[1]) - 1):
+        values = [eq[1][0]]
 
-            value = eq[1][0]
-
-            for i, operation in enumerate(operations):
-                match operation:
-                    case 0:
-                        value += eq[1][i+1]
-                    case 1:
-                        value = value*eq[1][i+1]
-                if value > eq[0]:
-                    break
-            
-            if value == eq[0]:
-                res += eq[0]
-                break
+        for m in eq[1][1:]:
+            new_values = [value + m for value in values] + [value * m for value in values]
+            values = new_values
+        
+        if eq[0] in values:
+            res += eq[0]
 
     print(res)
--- day07/day07_2.py
+++ day07/day07_2.py
@@ -1,9 +1,3 @@
-def generate_ternaries(n: int):
-    if n>1:
-        return [bin+[0] for bin in generate_ternaries(n-1)] + [bin+[1] for bin in generate_ternaries(n-1)] + [bin+[2] for bin in generate_ternaries(n-1)]
-    else:
-        return [[0], [1], [2]]
-
 with open("Advent-of-Code-2024/inputs/input07") as f:
     equations = [[int(eq[0]), [int(i) for i in eq[1].strip().split(' ')]] for eq in [l.strip().split(':') for l in f.readlines()]]
 
@@ -11,23 +5,13 @@ with open("Advent-of-Code-2024/inputs/input07") as f:
 
     for eq in equations:
 
-        for operations in generate_ternaries(len(eq[1]) - 1):
+        values = [eq[1][0]]
 
-            value = eq[1][0]
-
-            for i, operation in enumerate(operations):
-                match operation:
-                    case 0:
-                        value += eq[1][i+1]
-                    case 1:
-                        value = value*eq[1][i+1]
-                    case 2:
-                        value = int(str(value) + str(eq[1][i+1]))
-                if value > eq[0]:
-                    break
-            
-            if value == eq[0]:
-                res += eq[0]
-                break
+        for m in eq[1][1:]:
+            new_values = [value + m for value in values] + [value * m for value in values] + [int(str(value) + str(m)) for value in values]
+            values = new_values
+        
+        if eq[0] in values:
+            res += eq[0]
 
     print(res)
